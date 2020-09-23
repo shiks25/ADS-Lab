6b049256ce72c1fc3d5f6a6edbd21054b77f4fe6
Shikha N <56836929+shiks25@users.noreply.github.com>
2020-09-23 16:57:24 +0530

    prog

---
+++ Lab1/inser_end.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node  
+{  
+    public: 
+    int data;  
+    Node* next;
+};  
+Node* XOR (Node *a, Node *b)  
+{  
+    return (Node*) ((uintptr_t) (a) ^ (uintptr_t) (b));  
+}  
+
+void insertend(Node **head,int data){
+    Node *new_node=new Node();
+    new_node->data=data;
+    if(*head == NULL){
+        new_node->next=*head;
+        *head=new_node;
+    }
+    else{
+    Node *curr=*head;
+    Node *prev=NULL;
+    Node *nxt;
+    
+    while(XOR(prev,curr->next)!=NULL){
+        nxt=XOR(prev,curr->next);
+        prev=curr;
+        curr=nxt;
+    }
+    
+   new_node->next=curr;
+   curr->next=XOR(prev,new_node);
+   
+    }
+}
+    
+
+void printL(Node *head)  
+{  
+    Node *curr = head;  
+    Node *prev = NULL;  
+    Node *next;  
+  
+    cout << "The nodes in the Linked List are: \n";  
+  
+    while (curr != NULL)  
+    {  
+         
+        cout<<curr->data<<" ";  
+        next = XOR (prev, curr->next);  
+  
+        prev = curr;  
+        curr = next;  
+    }  
+}  
+int main()
+{
+    Node *head = NULL; 
+    int n;
+    cout<<"--Insert in the end--"<<endl;
+    cout<<"Enter the number of elements:"<<endl;
+    cin>>n;
+    cout<<"Enter the elements:"<<endl;
+    while(n){
+    int x;
+    cin>>x;
+    insertend(&head, x); 
+    n--;
+    }
+    printL(head);
+    return 0;
+}
